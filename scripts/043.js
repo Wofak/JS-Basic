af934175cf90fd17f6f0e422cfa8f605229dbcfb
Wofak <wodstra@protonmail.com>
2021-06-29 16:27:27 +0900

    JS Template Literals

---
+++ scripts/043.js
@@ -0,0 +1,13 @@
+//Template Literals
+
+// Hello Jeremey. Welcome!
+function hello(name) {
+    console.log("Hello" + name + ".Welcome!");
+}
+
+function hello2(name) {
+    console.log(`Hello ${name}. Welcome!`);
+}
+hello2("Jeremy");
+
+//${변수} 형식으로 사용
